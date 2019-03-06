b72b2696e8d9b03a2c1e4f0f5b372d850be7ba33
korsa719 <k.matlayev@gmail.com>
2019-03-06 13:50:36 +0200

    added field from mock to the update bean

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/bean/UserUpdateBean.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/bean/UserUpdateBean.java
@@ -1,5 +1,6 @@
 package tech.shooting.ipsc.bean;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -9,8 +10,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.Indexed;
+import tech.shooting.commons.enums.RoleName;
 import tech.shooting.ipsc.pojo.Address;
 import tech.shooting.ipsc.validator.ValidationConstants;
+
+import java.time.OffsetDateTime;
 
 @Data
 public class UserUpdateBean {
@@ -27,8 +32,27 @@ public class UserUpdateBean {
 	private String name;
 	
 	@JsonProperty
-	@ApiModelProperty(value = "User's address", required = true)
+	@ApiModelProperty(value = "User's address")
 	private Address address;
+
+	@JsonProperty
+	@ApiModelProperty(value = "User's login")
+	@NotEmpty(message = ValidationConstants.USER_LOGIN_MESSAGE)
+	private String login;
+
+	@JsonProperty
+	@ApiModelProperty(value = "User's password")
+	@NotEmpty(message = ValidationConstants.USER_PASSWORD_MESSAGE)
+	@Size(min = 4, message = ValidationConstants.USER_PASSWORD_MESSAGE)
+	private String password;
+
+	@JsonProperty
+	@ApiModelProperty(value = "User's birthday")
+	private OffsetDateTime birthDate;
+
+	@JsonProperty
+	@ApiModelProperty(value = "User's active ")
+	private boolean active;
 	
 	
 }
