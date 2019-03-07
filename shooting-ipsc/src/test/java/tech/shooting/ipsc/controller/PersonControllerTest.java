7fd9fd4de986cf441e5ed914336d6b8aad1be27d
korsa719 <k.matlayev@gmail.com>
2019-03-07 17:32:14 +0200

    create test

---
+++ shooting-ipsc/src/test/java/tech/shooting/ipsc/controller/PersonControllerTest.java
@@ -0,0 +1,97 @@
+package tech.shooting.ipsc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import tech.shooting.commons.constraints.IpscConstants;
+import tech.shooting.commons.enums.RoleName;
+import tech.shooting.commons.exception.ValidationException;
+import tech.shooting.commons.pojo.Token;
+import tech.shooting.commons.utils.JacksonUtils;
+import tech.shooting.ipsc.advice.ValidationErrorHandler;
+import tech.shooting.ipsc.config.IpscMongoConfig;
+import tech.shooting.ipsc.config.IpscSettings;
+import tech.shooting.ipsc.config.SecurityConfig;
+import tech.shooting.ipsc.db.DatabaseCreator;
+import tech.shooting.ipsc.pojo.Address;
+import tech.shooting.ipsc.pojo.User;
+import tech.shooting.ipsc.repository.PersonRepository;
+import tech.shooting.ipsc.repository.UserRepository;
+import tech.shooting.ipsc.security.IpscUserDetailsService;
+import tech.shooting.ipsc.security.TokenAuthenticationFilter;
+import tech.shooting.ipsc.security.TokenAuthenticationManager;
+import tech.shooting.ipsc.security.TokenUtils;
+
+import java.util.Date;
+import java.util.Map;
+
+@ExtendWith(SpringExtension.class)
+@EnableMongoRepositories(basePackageClasses = PersonRepository.class)
+@ContextConfiguration(classes = { ValidationErrorHandler.class, IpscSettings.class, IpscMongoConfig.class,
+    TokenUtils.class, SecurityConfig.class, DatabaseCreator.class, TokenAuthenticationManager.class,
+    TokenAuthenticationFilter.class, IpscUserDetailsService.class, PersonController.class,ValidationErrorHandler.class })
+@EnableAutoConfiguration
+@AutoConfigureMockMvc
+@SpringBootTest
+@DirtiesContext
+@Slf4j
+@Tag(IpscConstants.UNIT_TEST_TAG)
+public class PersonControllerTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    private User user;
+    private User admin;
+
+    private String adminToken;
+
+    private String userJson;
+
+    private String userToken;
+    @BeforeEach
+    public void before() {
+        String password = RandomStringUtils.randomAscii(14);
+        user = new User().setLogin(RandomStringUtils.randomAlphanumeric(15)).setName("Test firstname").setPassword(password).setRoleName(RoleName.USER).setAddress(new Address().setIndex("08150"));
+        admin = userRepository.findByLogin(DatabaseCreator.ADMIN_LOGIN);
+        userJson = JacksonUtils.getFullJson(user);
+
+        userToken = adminToken = tokenUtils.createToken(admin.getId(), Token.TokenType.USER, admin.getLogin(), RoleName.USER, DateUtils.addMonths(new Date(), 1), DateUtils.addDays(new Date(), -1));
+        adminToken = tokenUtils.createToken(admin.getId(), Token.TokenType.USER, admin.getLogin(), RoleName.ADMIN, DateUtils.addMonths(new Date(), 1), DateUtils.addDays(new Date(), -1));
+
+    }
+
+    @Test
+    public void checkCreatePerson(){
+
+    }
+
+}
