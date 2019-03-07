9fa08df3a2da016fc36c1a7bf54037765b3dda70
korsa719 <k.matlayev@gmail.com>
2019-03-07 14:56:36 +0200

    added person repository

---
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package tech.shooting.ipsc.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import tech.shooting.ipsc.pojo.Person;
+
+public interface PersonRepository extends MongoRepository <Person,Long> {
+
+}
