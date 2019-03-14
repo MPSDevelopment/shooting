af6fb277d8e2edd7726985248f53c55685ffc508
korsa719 <k.matlayev@gmail.com>
2019-03-14 15:50:40 +0200

    refactor controllerApi request

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/ControllerAPI.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/ControllerAPI.java
@@ -6,10 +6,10 @@ public class ControllerAPI {
 
 	public static final String COMPETITION_ID_REQUEST = "{competitionId}";
 	public static final String STAGE_ID_REQUEST = "{stageId}";
-	public static final String PAGE_NUMBER = "{pageNumber}";
-	public static final String PAGE_SIZE = "{pageSize}";
-	public static final String PERSON_ID = "{personId}";
-	public static final String USER_ID = "{userId}";
+	public static final String PAGE_NUMBER_REQUEST = "{pageNumber}";
+	public static final String PAGE_SIZE_REQUEST = "{pageSize}";
+	public static final String PERSON_ID_REQUEST = "{personId}";
+	public static final String USER_ID_REQUEST = "{userId}";
 	public static final String COMPETITOR_ID_REQUEST = "{competitorId}";
 
 	public static final String AUTH_CONTROLLER = "/api/auth";
@@ -19,23 +19,23 @@ public class ControllerAPI {
 	public static final String AUTH_CONTROLLER_POST_STATUS = "/status";
 
 	public static final String USER_CONTROLLER = "/api/users";
-	public static final String USER_CONTROLLER_PUT_USER = "/" + USER_ID;
-	public static final String USER_CONTROLLER_GET_USER = "/" + USER_ID;
-	public static final String USER_CONTROLLER_DELETE_USER = "/" + USER_ID;
-	public static final String USER_CONTROLLER_CHANGE_PASSWORD = "/changepassword/" + USER_ID;
-	public static final String USER_CONTROLLER_GET_USERS_BY_PAGE = "/all/" + PAGE_NUMBER + "/" + PAGE_SIZE;
+	public static final String USER_CONTROLLER_PUT_USER = "/" + USER_ID_REQUEST;
+	public static final String USER_CONTROLLER_GET_USER = "/" + USER_ID_REQUEST;
+	public static final String USER_CONTROLLER_DELETE_USER = "/" + USER_ID_REQUEST;
+	public static final String USER_CONTROLLER_CHANGE_PASSWORD = "/changepassword/" + USER_ID_REQUEST;
+	public static final String USER_CONTROLLER_GET_USERS_BY_PAGE = "/all/" + PAGE_NUMBER_REQUEST + "/" + PAGE_SIZE_REQUEST;
 	public static final String USER_CONTROLLER_GET_USERS = "/all";
 	public static final String USER_CONTROLLER_GET_COUNT = "/count";
 	public static final String USER_CONTROLLER_POST_USER = "/create";
 	public static final String USER_CONTROLLER_GET_JUDGES = "/all/judge";
 
 	public static final String PERSON_CONTROLLER = "/api/persons";
-	public static final String PERSON_CONTROLLER_GET_PERSON = "/" + PERSON_ID;
+	public static final String PERSON_CONTROLLER_GET_PERSON = "/" + PERSON_ID_REQUEST;
 	public static final String PERSON_CONTROLLER_POST_PERSON = "/create";
-	public static final String PERSON_CONTROLLER_PUT_PERSON = "/" + PERSON_ID;
-	public static final String PERSON_CONTROLLER_DELETE_PERSON = "/" + PERSON_ID;
+	public static final String PERSON_CONTROLLER_PUT_PERSON = "/" + PERSON_ID_REQUEST;
+	public static final String PERSON_CONTROLLER_DELETE_PERSON = "/" + PERSON_ID_REQUEST;
 	public static final String PERSON_CONTROLLER_GET_PERSONS = "/all";
-	public static final String PERSON_CONTROLLER_GET_USERS_BY_PAGE = "/all/" + PAGE_NUMBER + "/" + PAGE_SIZE;
+	public static final String PERSON_CONTROLLER_GET_USERS_BY_PAGE = "/all/" + PAGE_NUMBER_REQUEST + "/" + PAGE_SIZE_REQUEST;
 	public static final String PERSON_CONTROLLER_GET_COUNT = "/count";
 
 	public static final String COMPETITION_CONTROLLER = "/api/competition";
@@ -45,7 +45,7 @@ public class ControllerAPI {
 	public static final String COMPETITION_CONTROLLER_PUT_COMPETITION = "/" + COMPETITION_ID_REQUEST;
 	public static final String COMPETITION_CONTROLLER_GET_COUNT = "/count";
 	public static final String COMPETITION_CONTROLLER_GET_COMPETITIONS = "/all";
-	public static final String COMPETITION_CONTROLLER_GET_COMPETITION_BY_PAGE = "/all/" + PAGE_NUMBER + "/" + PAGE_SIZE;
+	public static final String COMPETITION_CONTROLLER_GET_COMPETITION_BY_PAGE = "/all/" + PAGE_NUMBER_REQUEST + "/" + PAGE_SIZE_REQUEST;
 
 	public static final String COMPETITION_CONTROLLER_GET_STAGES = "/" + COMPETITION_ID_REQUEST + "/stage/all";
 	public static final String COMPETITION_CONTROLLER_POST_STAGES = "/" + COMPETITION_ID_REQUEST + "/stages";
@@ -57,7 +57,7 @@ public class ControllerAPI {
 
 	public static final String COMPETITION_CONTROLLER_GET_COMPETITORS = "/" + COMPETITION_ID_REQUEST + "/competitor/all";
 	public static final String COMPETITION_CONTROLLER_POST_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor";
-	public static final String COMPETITION_CONTROLLER_DELETE_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor/" + COMPETITOR_ID_REQUEST;
-	public static final String COMPETITION_CONTROLLER_PUT_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor/" + COMPETITOR_ID_REQUEST;
 	public static final String COMPETITION_CONTROLLER_GET_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor/" + COMPETITOR_ID_REQUEST;
+	public static final String COMPETITION_CONTROLLER_PUT_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor/" + COMPETITOR_ID_REQUEST;
+	public static final String COMPETITION_CONTROLLER_DELETE_COMPETITOR = "/" + COMPETITION_ID_REQUEST + "/competitor/" + COMPETITOR_ID_REQUEST;
 }
