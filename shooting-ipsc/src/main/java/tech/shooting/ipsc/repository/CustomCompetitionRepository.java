6aaf31d4379484ff7d20c8fe227a2a9b7e7771fb
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-03-18 13:07:32 +0200

    Added id generation to a stage push

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CustomCompetitionRepository.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CustomCompetitionRepository.java
@@ -5,9 +5,12 @@ import tech.shooting.ipsc.pojo.Stage;
 
 public interface CustomCompetitionRepository  {
 
-	Competition getByStageId(Long id);
-	Stage getStageById(Long id);
-	public void pushStageToCompetition(Long competitionId, Stage stage);
+	public Competition getByStageId(Long id);
+	
+	public Stage getStageById(Long id);
+	
+	public Stage pushStageToCompetition(Long competitionId, Stage stage);
+	
 	public void pullStageFromCompetition(Long competitionId, Stage stage);
 
 }
--- shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CustomCompetitionRepositoryImpl.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CustomCompetitionRepositoryImpl.java
@@ -6,22 +6,25 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+
+import com.mpsdevelopment.plasticine.commons.IdGenerator;
+
 import tech.shooting.ipsc.pojo.Competition;
 import tech.shooting.ipsc.pojo.Stage;
 
-@Slf4j
-public class CustomCompetitionRepositoryImpl implements CustomCompetitionRepository {
+@Slf4j class CustomCompetitionRepositoryImpl implements CustomCompetitionRepository {
+	
 	@Autowired
 	private MongoTemplate mongoTemplate;
 
 	@Override
-	public Competition getByStageId (Long id) {
+	public Competition getByStageId(Long id) {
 		Query query = Query.query(Criteria.where(Competition.STAGES_FIELD + "." + Competition.ID_FIELD).is(id));
 		return mongoTemplate.findOne(query, Competition.class);
 	}
 
 	@Override
-	public Stage getStageById (Long id) {
+	public Stage getStageById(Long id) {
 		Query query = Query.query(Criteria.where(Competition.STAGES_FIELD + "." + Competition.ID_FIELD).is(id));
 		query.fields().include(Competition.ID_FIELD).include(Competition.NAME_FIELD).position(Competition.STAGES_FIELD, 1);
 		Competition competition = mongoTemplate.findOne(query, Competition.class);
@@ -29,12 +32,16 @@ public class CustomCompetitionRepositoryImpl implements CustomCompetitionReposit
 	}
 
 	@Override
-	public void pushStageToCompetition (Long competitionId, Stage stage) {
+	public Stage pushStageToCompetition(Long competitionId, Stage stage) {
+		if (stage.getId() == null) {
+			stage.setId(IdGenerator.nextId());
+		}
 		mongoTemplate.updateFirst(Query.query(Criteria.where(Competition.ID_FIELD).is(competitionId)), new Update().push(Competition.STAGES_FIELD, stage), Competition.class);
+		return stage;
 	}
 
 	@Override
-	public void pullStageFromCompetition (Long competitionId, Stage stage) {
+	public void pullStageFromCompetition(Long competitionId, Stage stage) {
 		// mongoTemplate.updateFirst(Query.query(Criteria.where("id").is(competitionId)), new Update().pull("stages", stage.getId()), Competition.class);
 		mongoTemplate.updateFirst(Query.query(Criteria.where(Competition.ID_FIELD).is(competitionId)), new Update().pull(Competition.STAGES_FIELD, Query.query(Criteria.where(Competition.ID_FIELD).is(stage.getId()))), Competition.class);
 	}
