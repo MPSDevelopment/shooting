7802f3acecbdc46dcbce2d378b4ed301571c3168
korsa719 <k.matlayev@gmail.com>
2019-03-29 11:42:09 +0200

    describe SubjectName fields

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/SubjectsName.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/SubjectsName.java
@@ -1,12 +1,18 @@
 package tech.shooting.ipsc.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
 public class SubjectsName {
+	@JsonProperty
+	@ApiModelProperty("Constant name short")
 	private String useName;
 
+	@JsonProperty
+	@ApiModelProperty("Full name")
 	private String name;
 }
