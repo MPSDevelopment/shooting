e9e7fa17aad811ac016b8225d99219d15dd42d6b
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-04-09 16:10:28 +0300

    Added some validation fields and validation export annotation

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/Answer.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/Answer.java
@@ -4,12 +4,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import tech.shooting.commons.annotation.ValiationExportable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Accessors(chain = true)
-public class Answer {
+public class Answer implements ValiationExportable {
+
+	@JsonProperty
+	@ApiModelProperty(value = "Answer number", required = true)
+	private Integer number;
+
 	@JsonProperty
 	@ApiModelProperty(value = "Answer in rus", required = true)
 	@NotBlank
--- shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/Question.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/Question.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Question extends BaseDocument {
 	@JsonProperty
 	@ApiModelProperty(value = "List of choice", required = true)
-	@Size(min = 4, message = ValidationConstants.ANSWERS_SIZE_MESSAGE)
+	@Size(min = 2, max = 4, message = ValidationConstants.ANSWERS_SIZE_MESSAGE)
 	List<Answer> answers;
 
 	@JsonProperty
