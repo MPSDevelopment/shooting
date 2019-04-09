434f947cc303b7ba5d41025a2fcaeecc2cbddf4b
korsa719 <k.matlayev@gmail.com>
2019-04-09 18:04:28 +0300

    cover @Test checkFindByDivision Person repository

--- shooting-ipsc/src/test/java/tech/shooting/ipsc/repository/PersonRepositoryTest.java
+++ shooting-ipsc/src/test/java/tech/shooting/ipsc/repository/PersonRepositoryTest.java
@@ -14,9 +14,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tech.shooting.commons.constraints.IpscConstants;
 import tech.shooting.ipsc.config.IpscMongoConfig;
+import tech.shooting.ipsc.pojo.Division;
 import tech.shooting.ipsc.pojo.Person;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -32,16 +34,29 @@ public class PersonRepositoryTest {
 	@Autowired
 	private PersonRepository personRepository;
 
+	@Autowired
+	private DivisionRepository divisionRepository;
 	@BeforeEach
 	public void before () {
 		personRepository.deleteAll();
 	}
 
 	@Test
-	public void checkFindByNameAndCodeIPSC () {
+	public void checkFindByNameAndBirthDate () {
 		String name = "Tigran";
 		OffsetDateTime offsetDateTime = OffsetDateTime.now();
 		personRepository.save(new Person().setName(name).setBirthDate(offsetDateTime));
 		assertNotNull(personRepository.findByNameAndBirthDate(name, offsetDateTime));
+	}
+
+	@Test
+	public void checkFindByDivision () {
+		String name = "Tigran";
+		OffsetDateTime offsetDateTime = OffsetDateTime.now();
+		List<Division> all = divisionRepository.findAll();
+		Division division = all.get(0);
+		log.info("Set division %s", division);
+		personRepository.save(new Person().setName(name).setBirthDate(offsetDateTime).setDivision(division));
+		assertNotNull(personRepository.findByDivision(division));
 	}
 }
