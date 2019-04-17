023a0b3e1ae962b6b63e8eb253e75281b32df4e3
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-04-17 15:45:59 +0300

    Improved TypeOfPresence

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/enums/TypeOfPresence.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/enums/TypeOfPresence.java
@@ -6,33 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum TypeOfPresence {
-	ALL(0, "All"), 
-	UNDEFINED(1, "Unidentified location"),
-	MISSION(2, "In official journey"),
-	SICK_LEAVE(3, "Home because is sick"),
-	INFIRMARY(4, "In infirmary"),
-	HOSP(5, "In hospital"),
-	OUTFIT(6, "In order"),
-	DAY_OFF(7, "Day off"),
-	PRESENT(8, "Is present"),
-	DELAY(9, "Be late");
+	ALL("ALL"), 
+	UNDEFINED("UNDEFINED"),
+	MISSION("MISSION"),
+	SICK_LEAVE("SICK_LEAVE"),
+	INFIRMARY("INFIRMARY"),
+	HOSPITAL("HOSPITAL"),
+	OUTFIT("OUTFIT"),
+	DAY_OFF("DAY_OFF"),
+	PRESENT("PRESENT"),
+	DELAY("DELAY");
 
 	private String state;
-
-	private Integer id;
 
 	TypeOfPresence () {
 	}
 
-	TypeOfPresence (Integer id, String state) {
+	TypeOfPresence (String state) {
 		this.state = state;
-		this.id = id;
 	}
 
 	public static List<TypePresent> getList () {
 		List<TypePresent> result = new ArrayList<>();
 		for(TypeOfPresence value : TypeOfPresence.values()) {
-			result.add(new TypePresent().setId(value.id).setState(value.getState()));
+			result.add(new TypePresent().setState(value.getState()));
 		}
 		return result;
 	}
@@ -64,9 +61,5 @@ public enum TypeOfPresence {
 
 	public String getState () {
 		return state;
-	}
-
-	public int getId () {
-		return id;
 	}
 }
