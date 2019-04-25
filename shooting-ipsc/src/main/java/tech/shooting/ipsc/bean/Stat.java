32739d260aff24e680a63ac0c183ac7f1184afc3
dmitriy.yavorskiy <dmitriy.yavorskiy@gmail.com>
2019-04-25 11:58:45 +0300

    Fixed statsList

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/bean/Stat.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/bean/Stat.java
@@ -1,5 +1,7 @@
 package tech.shooting.ipsc.bean;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 import tech.shooting.ipsc.enums.TypeOfPresence;
@@ -7,8 +9,11 @@ import tech.shooting.ipsc.enums.TypeOfPresence;
 @Data
 @Accessors(chain = true)
 public class Stat {
+	
+	@JsonProperty
 	private TypeOfPresence status;
 
+	@JsonProperty
 	private Integer count;
 
 	@Override
