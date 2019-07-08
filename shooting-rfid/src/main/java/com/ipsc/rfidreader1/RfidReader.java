7b0c727d688139c86ca1c2286f91446206dc56de
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-07-08 13:18:25 +0300

    Added Rfid manager initialization

--- shooting-rfid/src/main/java/com/ipsc/rfidreader1/RfidReader.java
+++ shooting-rfid/src/main/java/com/ipsc/rfidreader1/RfidReader.java
@@ -4,17 +4,23 @@ import com.handheld.UHF.UhfManager;
 
 import java.util.List;
 
+import cn.pda.serialport.SerialPort;
 import cn.pda.serialport.Tools;
 
 public class RfidReader {
 
 	private UhfManager manager = UhfManager.getInstance();
 
+	private int sensitive = 0;
+	private int power = 0;// rate of work
+	private int area = 0;
+	private int frequency = 0;
+
 	public String show() {
 		String rfidMark = "";
 
 		List<byte[]> epcList;
-		
+
 		epcList = manager.inventoryRealTime();
 
 		if (epcList != null && !epcList.isEmpty()) {
@@ -23,6 +29,59 @@ public class RfidReader {
 			}
 		}
 
-		return "Here must be mark" + rfidMark ;
+		return "Here must be mark -> " + rfidMark;
+	}
+
+	public String initialize() {
+
+		String powerString = "";
+		switch (UhfManager.Power) {
+		case SerialPort.Power_3v3:
+			powerString = "power_3V3";
+			break;
+		case SerialPort.Power_5v:
+			powerString = "power_5V";
+			break;
+		case SerialPort.Power_Scaner:
+			powerString = "scan_power";
+			break;
+		case SerialPort.Power_Psam:
+			powerString = "psam_power";
+			break;
+		case SerialPort.Power_Rfid:
+			powerString = "rfid_power";
+			break;
+		default:
+			break;
+		}
+		manager = UhfManager.getInstance();
+		if (manager == null) {
+			return "Serial port failed";
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+//		Log.e("", "value" + power);
+		manager.setOutputPower(power);
+		manager.setWorkArea(area);
+		byte[] version_bs = manager.getFirmware();
+		if (version_bs != null) {
+			return "Manager has been initiated " + new String(version_bs) + ". Power string is " + powerString;
+		}
+
+		return "Manager has not been initiated" + ". Power string is " + powerString;
+	}
+
+	protected void pause() {
+		manager.close();
+	}
+
+	protected void destroy() {
+		if (manager != null) {
+			manager.close();
+		}
 	}
 }
