ca6d5e1cd12fba258d2ac71ba938adb46641b45d
dmitriy.yavorskiy <dmitriy.yavorskiy@gmail.com>
2019-08-14 21:33:04 -0500

    Added more division repository methods for cources

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CourseRepository.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/repository/CourseRepository.java
@@ -1,15 +1,23 @@
 package tech.shooting.ipsc.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import tech.shooting.ipsc.pojo.Course;
+import tech.shooting.ipsc.pojo.Division;
 import tech.shooting.ipsc.pojo.Person;
 
 import java.util.List;
 
 @Repository
-public interface CourseRepository extends MongoRepository<Course,Long> {
-    List<Course> findAllByDivision(Long division);
+public interface CourseRepository extends MongoRepository<Course, Long> {
 
-    List<Course> findAllByPerson(Person person);
+	List<Course> findAllByDivision(Long division);
+
+	List<Course> findAllByPerson(Person person);
+
+	List<Course> findByDivisionIn(List<Long> list);
+
+	Page<Course> findByDivisionIn(List<Long> list, PageRequest pageable);
 }
