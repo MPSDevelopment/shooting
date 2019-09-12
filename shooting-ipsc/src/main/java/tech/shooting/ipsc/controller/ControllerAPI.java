44333cc0f38d566eab1898c108ae78628445e2a0
dmitriy.yavorskiy <dmitriy.yavorskiy@gmail.com>
2019-09-12 12:00:27 +0300

    Fixed slashes in apis

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/ControllerAPI.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/ControllerAPI.java
@@ -248,7 +248,7 @@ public class ControllerAPI {
 	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_GET_ALL = "/get/all";
 	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_GET_BY_ID = "/" + REQUEST_TYPE_ID;
 	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_POST_TYPE = "/create";
-	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_PUT_TYPE = "/update" + REQUEST_TYPE_ID;
+	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_PUT_TYPE = "/update/" + REQUEST_TYPE_ID;
 	public static final String COMMUNICATION_EQUIPMENT_TYPE_CONTROLLER_DELETE_TYPE_BY_ID = "/delete/" + REQUEST_TYPE_ID;
 	
 	// vehicle type section
@@ -256,7 +256,7 @@ public class ControllerAPI {
 	public static final String VEHICLE_TYPE_CONTROLLER_GET_ALL = "/get/all";
 	public static final String VEHICLE_TYPE_CONTROLLER_GET_BY_ID = "/" + REQUEST_TYPE_ID;
 	public static final String VEHICLE_TYPE_CONTROLLER_POST_TYPE = "/create";
-	public static final String VEHICLE_TYPE_CONTROLLER_PUT_TYPE = "/update" + REQUEST_TYPE_ID;
+	public static final String VEHICLE_TYPE_CONTROLLER_PUT_TYPE = "/update/" + REQUEST_TYPE_ID;
 	public static final String VEHICLE_TYPE_CONTROLLER_DELETE_TYPE_BY_ID = "/delete/" + REQUEST_TYPE_ID;
 
 	// workspace
