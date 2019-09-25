a6f277115317942ec9ff2cef97c89a8219f19477
dmitriy.yavorskiy <dmitriy.yavorskiy@gmail.com>
2019-09-25 08:51:52 +0300

    Prepare to the excelsior jet build

--- shooting-ipsc/src/test/java/tech/shooting/ipsc/reader/TagReaderApplication.java
+++ shooting-ipsc/src/test/java/tech/shooting/ipsc/reader/TagReaderApplication.java
@@ -1,26 +1,26 @@
-package tech.shooting.ipsc.reader;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.impinj.octane.ImpinjReader;
-
-import lombok.extern.slf4j.Slf4j;
-
-@SpringBootApplication
-@Slf4j
-public class TagReaderApplication implements CommandLineRunner {
-
-	ImpinjReader impinjReader;
-
-	public static void main(String[] args) {
-		SpringApplication.run(TagReaderApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Reading imping reader");
-		impinjReader = new ImpinjReader();
-	}
-}
+//package tech.shooting.ipsc.reader;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import com.impinj.octane.ImpinjReader;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+//@SpringBootApplication
+//@Slf4j
+//public class TagReaderApplication implements CommandLineRunner {
+//
+//	ImpinjReader impinjReader;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TagReaderApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		log.info("Reading imping reader");
+//		impinjReader = new ImpinjReader();
+//	}
+//}
