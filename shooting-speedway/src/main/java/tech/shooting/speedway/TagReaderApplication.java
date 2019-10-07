624406482c6ebd8c2eabceb98132b9920a517a5d
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-10-07 20:18:45 +0300

    Tested reader

--- shooting-speedway/src/main/java/tech/shooting/speedway/TagReaderApplication.java
+++ shooting-speedway/src/main/java/tech/shooting/speedway/TagReaderApplication.java
@@ -1,25 +1,19 @@
 package tech.shooting.speedway;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.impinj.octane.ImpinjReader;
+import com.impinj.octane.OctaneSdkException;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootApplication
-public class TagReaderApplication implements CommandLineRunner {
+@Slf4j
+public class TagReaderApplication {
 
-	ImpinjReader impinjReader;
+	static ImpinjReader impinjReader;
 
-	public static void main(String[] args) {
-		SpringApplication.run(TagReaderApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Reading imping reader");
+	public static void main(String[] args) throws OctaneSdkException {
 		impinjReader = new ImpinjReader();
+		log.info("Reader has been started");
+
+		impinjReader.connect("192.168.0.1");
 	}
 }
