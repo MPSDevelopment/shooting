329e6cf83ea5ebd95ccab8ee017f76db223cc16b
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-10-24 20:48:40 +0300

    Added log for quia dates range

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/service/QuizService.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/service/QuizService.java
@@ -29,10 +29,10 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class QuizService {
-	
+
 	@Autowired
 	private QuizRepository quizRepository;
-	
+
 	@Autowired
 	private QuizScoreRepository quizScoreRepository;
 
@@ -42,90 +42,90 @@ public class QuizService {
 	@Autowired
 	private PersonRepository personRepository;
 
-	public Quiz createQuiz (QuizBean quiz) throws BadRequestException {
+	public Quiz createQuiz(QuizBean quiz) throws BadRequestException {
 		Quiz quizToDB = new Quiz();
 		BeanUtils.copyProperties(quiz, quizToDB, Quiz.SUBJECT);
 		quizToDB.setSubject(checkSubject(quiz.getSubject()));
 		return quizRepository.save(quizToDB);
 	}
 
-	private Subject checkSubject (Long subject) throws BadRequestException {
+	private Subject checkSubject(Long subject) throws BadRequestException {
 		return subjectRepository.findById(subject).orElseThrow(() -> new BadRequestException(new ErrorMessage("Incorrect subject id %s", subject)));
 	}
 
-	public List<Subject> getEnum () {
+	public List<Subject> getEnum() {
 		return subjectRepository.findAll();
 	}
 
-	public List<Quiz> getAllQuiz () {
+	public List<Quiz> getAllQuiz() {
 		return quizRepository.findAll();
 	}
 
-	public Quiz getQuiz (Long id) throws BadRequestException {
+	public Quiz getQuiz(Long id) throws BadRequestException {
 		return checkQuiz(id);
 	}
 
-	private Quiz checkQuiz (Long id) throws BadRequestException {
+	private Quiz checkQuiz(Long id) throws BadRequestException {
 		return quizRepository.findById(id).orElseThrow(() -> new BadRequestException(new ErrorMessage("Incorrect quiz id %s", id)));
 	}
 
-	public Quiz updateQuiz (Long id, QuizBean quizBean) throws BadRequestException {
+	public Quiz updateQuiz(Long id, QuizBean quizBean) throws BadRequestException {
 		Quiz quiz = checkQuiz(id);
 		BeanUtils.copyProperties(quizBean, quiz);
 		return quizRepository.save(quiz);
 	}
 
-	public void removeQuiz (Long id) throws BadRequestException {
+	public void removeQuiz(Long id) throws BadRequestException {
 		quizRepository.delete(checkQuiz(id));
 	}
 
-	public Question addQuestion (Long id, Question question) throws BadRequestException {
+	public Question addQuestion(Long id, Question question) throws BadRequestException {
 		Quiz quiz = checkQuiz(id);
 		return quizRepository.pushQuestionToQuiz(quiz.getId(), question);
 	}
 
-	public Question getQuestion (Long id, Long questionId) throws BadRequestException {
+	public Question getQuestion(Long id, Long questionId) throws BadRequestException {
 		Quiz quiz = checkQuiz(id);
 		return checkQuestion(quiz, questionId);
 	}
 
-	private Question checkQuestion (Quiz quiz, Long questionId) throws BadRequestException {
+	private Question checkQuestion(Quiz quiz, Long questionId) throws BadRequestException {
 		return quiz.getQuestionList().stream().filter(ask -> ask.getId().equals(questionId)).findFirst().orElseThrow(() -> new BadRequestException(new ErrorMessage("Incorrect question id %s", questionId)));
 	}
 
-	public void deleteQuestion (Long id, Long questionId) {
+	public void deleteQuestion(Long id, Long questionId) {
 		quizRepository.pullQuestion(id, questionId);
 	}
 
-	public Question updateQuestion (Long id, Long questionId, Question question) throws BadRequestException {
+	public Question updateQuestion(Long id, Long questionId, Question question) throws BadRequestException {
 		Quiz quiz = checkQuiz(id);
 		checkQuestion(quiz, questionId);
 		quizRepository.pullQuestion(quiz.getId(), questionId);
 		return quizRepository.pushQuestionToQuiz(quiz.getId(), question);
 	}
 
-	public List<Quiz> getQuizFromSubject (Long subject) {
+	public List<Quiz> getQuizFromSubject(Long subject) {
 		return quizRepository.findBySubject(subject);
 	}
 
-	public ResponseEntity getQuizByPage (Integer page, Integer size) {
+	public ResponseEntity getQuizByPage(Integer page, Integer size) {
 		return Pageable.getPage(page, size, quizRepository);
 	}
 
-	public List<QuizScore> postScore (List<QuizScoreBean> listResult) throws BadRequestException {
+	public List<QuizScore> postScore(List<QuizScoreBean> listResult) throws BadRequestException {
 		List<QuizScore> reports = new ArrayList<>();
-		for(int i = 0; i < listResult.size(); i++) {
+		for (int i = 0; i < listResult.size(); i++) {
 			reports.add(checkScore(listResult.get(i)));
 		}
 		return reports;
 	}
 
-	private QuizScore checkScore (QuizScoreBean reportBean) throws BadRequestException {
-		//check quiz and person exist
+	private QuizScore checkScore(QuizScoreBean reportBean) throws BadRequestException {
+		// check quiz and person exist
 		Quiz quiz = checkQuiz(reportBean.getQuizId());
 		Person person = checkPerson(reportBean.getPerson());
-		//get list question from quiz where status is active
-		List<Question> collect = quiz.getQuestionList().stream().filter(Question :: isActive).collect(Collectors.toList());
+		// get list question from quiz where status is active
+		List<Question> collect = quiz.getQuestionList().stream().filter(Question::isActive).collect(Collectors.toList());
 		int questionCount = collect.size();
 		int rightAnswers = 0;
 		int incorrectAnswers = 0;
@@ -133,9 +133,9 @@ public class QuizService {
 //		List<Row> incorrect = new ArrayList<>();
 //		List<Ask> skip = new ArrayList<>();
 		List<RowBean> list = reportBean.getList();
-		for(int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			Question question = checkQuestion(quiz, list.get(i).getQuestionId());
-			if(question.getRight() == list.get(i).getAnswer()) {
+			if (question.getRight() == list.get(i).getAnswer()) {
 				rightAnswers++;
 			} else {
 //				Row row = new Row();
@@ -145,56 +145,59 @@ public class QuizService {
 			}
 			collect.remove(question);
 		}
-		for(int i = 0; i < collect.size(); i++) {
+		for (int i = 0; i < collect.size(); i++) {
 //			skip.add(collect.get(i).getQuestion());
 			skippedAnswers++;
 		}
 		double percentage = calculatePercentage(rightAnswers, questionCount);
 		int score;
-		if(percentage >= quiz.getGreat()) {
+		if (percentage >= quiz.getGreat()) {
 			score = 5;
-		} else if(percentage >= quiz.getGood()) {
+		} else if (percentage >= quiz.getGood()) {
 			score = 4;
-		} else if(percentage >= quiz.getSatisfactorily()) {
+		} else if (percentage >= quiz.getSatisfactorily()) {
 			score = 3;
 		} else {
 			score = 2;
 		}
-		
+
 		QuizScore report = new QuizScore().setQuizId(quiz.getId()).setPerson(person).setScore(score).setCorrect(rightAnswers).setIncorrect(incorrectAnswers).setSkip(skippedAnswers).setTotal(questionCount);
-		
+
 		log.info("Quiz score is %s ", report);
-		
+
 		EventBus.publishEvent(new TestFinishedEvent(report));
-		
+
 		return quizScoreRepository.save(report);
 	}
 
-	private Person checkPerson (Long person) throws BadRequestException {
+	private Person checkPerson(Long person) throws BadRequestException {
 		return personRepository.findById(person).orElseThrow(() -> new BadRequestException(new ErrorMessage("Incorrect person id %s", person)));
 	}
 
-	private double calculatePercentage (double obtained, double total) {
+	private double calculatePercentage(double obtained, double total) {
 		return (obtained / total) * 100;
 	}
 
-	public List<QuestionBean> getQuestionList (Long id) throws BadRequestException {
+	public List<QuestionBean> getQuestionList(Long id) throws BadRequestException {
 		Quiz quiz = checkQuiz(id);
-		List<Question> collect = quiz.getQuestionList().stream().filter(Question :: isActive).collect(Collectors.toList());
+		List<Question> collect = quiz.getQuestionList().stream().filter(Question::isActive).collect(Collectors.toList());
 		return convertToListQuestionsBean(collect);
 	}
 
-	private List<QuestionBean> convertToListQuestionsBean (List<Question> collect) {
+	private List<QuestionBean> convertToListQuestionsBean(List<Question> collect) {
 		List<QuestionBean> res = new ArrayList<>();
-		for(int i = 0; i < collect.size(); i++) {
+		for (int i = 0; i < collect.size(); i++) {
 			QuestionBean bean = new QuestionBean();
 			BeanUtils.copyProperties(collect.get(i), bean);
 			res.add(bean);
 		}
 		return res;
 	}
-	
+
 	public List<QuizScore> getScoreList(QuizScoreRequest query) throws BadRequestException {
+
+		log.info("Quiz score request dates are (%s:%s)", query.getStartDate(), query.getEndDate());
+
 		if (query.getQuizId() != null) {
 			checkQuiz(query.getQuizId());
 		}
