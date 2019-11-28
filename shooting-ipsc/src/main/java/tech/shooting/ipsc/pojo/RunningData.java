b8ad9d6793837407f00701ae3b52e25cde71ed4e
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-11-28 14:01:51 +0200

    Fixed running data issues

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/RunningData.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/pojo/RunningData.java
@@ -1,5 +1,7 @@
 package tech.shooting.ipsc.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -7,11 +9,15 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class RunningData {
 
+	@JsonProperty
 	private Person person;
 	
+	@JsonProperty
 	private long firstTime;
 	
+	@JsonProperty
 	private long lastTime;
 	
+	@JsonProperty
 	private int laps = 0;
 }
