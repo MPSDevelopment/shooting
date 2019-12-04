299576138fe6d351f21cd2039839178d40cc58f0
dmitriyyavorskiy <dmitriy.yavorskiy@gmail.com>
2019-12-04 21:29:34 +0200

    Added png to map image

--- shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/MapController.java
+++ shooting-ipsc/src/main/java/tech/shooting/ipsc/controller/MapController.java
@@ -24,10 +24,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.mpsdevelopment.plasticine.commons.IdGenerator;
 
-import tech.shooting.ipsc.bean.UploadFileBean;
 import tech.shooting.ipsc.bean.UploadMapBean;
 import tech.shooting.ipsc.pojo.Image;
-import tech.shooting.ipsc.service.ImageService;
 import tech.shooting.ipsc.service.MapService;
 import tech.shooting.ipsc.service.TileService;
 
@@ -40,34 +38,34 @@ public class MapController {
 
 	@Autowired
 	private TileService tileService;
-	
+
 	@Autowired
 	private MapService mapService;
 
 	@GetMapping(value = ControllerAPI.VERSION_1_0 + ControllerAPI.MAP_CONTROLLER_GET_TILE_URL, produces = MediaType.IMAGE_PNG_VALUE)
 	@ResponseBody
 	@ApiOperation(value = "Get Tile Image", notes = "Returns tile image by x, y and zoom")
-	public ResponseEntity<byte[]> getTile(@PathVariable(value = ControllerAPI.PATH_VARIABLE_ID) String id, @PathVariable(value = ControllerAPI.PATH_VARIABLE_Z) int zoom, @PathVariable(value = ControllerAPI.PATH_VARIABLE_X) int tileX, @PathVariable(value = ControllerAPI.PATH_VARIABLE_Y) int tileY) {
+	public ResponseEntity<byte[]> getTile(@PathVariable(value = ControllerAPI.PATH_VARIABLE_ID) String id, @PathVariable(value = ControllerAPI.PATH_VARIABLE_Z) int zoom, @PathVariable(value = ControllerAPI.PATH_VARIABLE_X) int tileX,
+			@PathVariable(value = ControllerAPI.PATH_VARIABLE_Y) int tileY) {
 		return getTileAsByteArray(id, zoom, tileX, tileY);
 	}
 
 	private ResponseEntity<byte[]> getTileAsByteArray(String id, int zoom, int tileX, int tileY) {
-        try {
-            byte[] body = FileUtils.readFileToByteArray(tileService.getTileImage(id, tileX, tileY, zoom));
-            return ResponseEntity.ok().cacheControl(CacheControl.maxAge(60, TimeUnit.MINUTES)).body(body);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try {
+			byte[] body = FileUtils.readFileToByteArray(tileService.getTileImage(id, tileX, tileY, zoom));
+			return ResponseEntity.ok().cacheControl(CacheControl.maxAge(60, TimeUnit.MINUTES)).body(body);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping(value = ControllerAPI.VERSION_1_0, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ApiOperation(value = "Post New Map")
 	public ResponseEntity<UploadMapBean> postMap(@RequestParam("file") MultipartFile file) throws IOException {
-		Image image = mapService.saveMap(file, String.valueOf(IdGenerator.nextId()));
+		Image image = mapService.saveMap(file, String.valueOf(IdGenerator.nextId()) + ".png");
 		return ResponseEntity.ok().body(new UploadMapBean(image.getFileName(), "Map %s has been uploaded", image.getFileName()));
 	}
-	
-	
+
 }
