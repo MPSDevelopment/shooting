b0f2232688e3c4a1cb05e45606abcc2c9fed1e97
dmitriy.yavorskiy <dmitriy.yavorskiy@gmail.com>
2020-01-23 17:01:42 +0800

    Added first zeros removal for a rfid reader

--- shooting-rfid/src/test/java/com/ipsc/rfidreader/RfidReaderTest.java
+++ shooting-rfid/src/test/java/com/ipsc/rfidreader/RfidReaderTest.java
@@ -6,16 +6,33 @@ import cn.pda.serialport.Tools;
 
 public class RfidReaderTest {
 	
+	private static final String code = "12345678";
+	
+	private static final String code3 = "08Е3";
+	
 	private byte[] accessPassword = { 0, 0, 0, 0};
 
 	@Test
 	public void checkTools() {
-		byte[] bytes = Tools.HexString2Bytes("12345678");
+		byte[] bytes = Tools.HexString2Bytes(code);
 
 		System.out.println(Tools.Bytes2HexString(bytes, bytes.length));
 		System.out.println(new String(bytes));
 
-		System.out.println(Tools.Bytes2HexString(accessPassword, accessPassword.length));
-
+	}
+	
+	@Test
+	public void checkTools3Symbols() {
+		byte[] bytes = { 8, 13, 3};
+		
+		System.out.println(Tools.Bytes2HexString(bytes, bytes.length));
+	}
+	
+	@Test
+	public void checkReplace() {
+		String code = code3.replaceFirst("0", "");
+		System.out.println(code);
+		code = "080E3".replaceFirst("0", "");
+		System.out.println(code);
 	}
 }
